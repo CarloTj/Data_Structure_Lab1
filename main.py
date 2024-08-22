051180c0b11984ddfa7586489a233f1998b5b766
Carlo E. Bilbao <c.bilbao@cfci.ph.educatiom>
2024-08-22 12:14:34 +0800

    Beginning

---
+++ main.py
@@ -0,0 +1,18 @@
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+# Num1 = int(num1)
+# Num2 = int(num2)
+if num1 > num2:
+    print(num1 + " is Greater Than " + num2)
+elif num1 < num2:
+    print(num1 + " is Less Than " + num2)
+elif num1 == num2:
+    print(num1 + " is equal to " + num2)
+else:
+    print("Error!")
+
+# name1 = input("Enter Classmate name 1: ")
+# name2 = input("Enter Classmate name 2: ")
+# name3 = input("Enter Classmate name 3: ")
+#
+# print(f"Your Classmates are {name1} {name2} {name3}")
